bdb73897fc1ac15e6409f23f6003f8722f8b20e2
Makoto1018 <105577978+Makoto1018@users.noreply.github.com>
2024-12-23 09:33:25 +0800

    Create 17_d226.cpp

---
+++ solutions/17_d226.cpp
@@ -0,0 +1,14 @@
+/* 10071 - Back to High School Physics https://zerojudge.tw/ShowProblem?problemid=d226 */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int t=0,v=0;
+    while(cin>>t>>v){
+        cout << v * t * 2 << endl;
+    }
+    return 0;
+}

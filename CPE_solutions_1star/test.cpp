c1de1665479d885f8723af3490ef5352bee44128
Makoto1018 <20021018ethan@gmail.com>
2025-08-05 22:19:41 +0800

    Revert "Update test.cpp"
    This reverts commit 81631f0a03e43533f2b030fac940223ad1435b42.

--- CPE_solutions_1star/test.cpp
+++ CPE_solutions_1star/test.cpp
@@ -1,34 +1,6 @@
-// ver 1.0
 #include <iostream>
 using namespace std;
-
 int main(){
-    int a, b;
-    char c;
-    cin >> a >> c >> b;
-    if (c =='+'){
-        add(a, b);
-    }
-    else if (c =='-'){
-        subtract(a, b);
-    }
-    else if (c == '*'){
-        cout << a * b << endl;
-    }
-    else if (c == '/'){
-        if (b == 0) {
-            cout << "Error: Division by zero" << endl;
-            return 1;
-        }
-        cout << a / b << endl;
-    }
+    cout <<"hello world!";
     return 0;
-}
-
-void add(int a, int b) {
-    cout << a + b << endl; // Corrected the comment syntax
-}
-
-void subtract(int a, int b) {
-    cout << a - b << endl;
 }
